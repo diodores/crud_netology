fc63d181e605e7607c3415b45a07938f7b733657
diodores <s9634898702@gmail.com>
2022-07-15 18:09:20 +0300

    'SEARCH_PARAM': 'products'

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -135,6 +135,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'SEARCH_PARAM': 'products',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1
 }
